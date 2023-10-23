170f22ee976c6b29b0c9fb4d8aeb92c460faccd7
NHJeans <guswo1013@naver.com>
2023-10-23 18:10:47 +0900

    :wrench: : history module 수정

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -4,11 +4,10 @@ import { HistoryService } from './history.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
-import { HistorySeedService } from './history.service.seed';
 
 @Module({
   imports: [AuthModule, PrismaModule, UserModule],
   controllers: [HistoryController],
-  providers: [HistoryService, HistorySeedService],
+  providers: [HistoryService],
 })
 export class HistoryModule {}
