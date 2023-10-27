c18b83bd5e8ccf5befab3592472210346998ff7e
NHJeans <guswo1013@naver.com>
2023-10-28 00:41:33 +0900

    :wrench: : 이미지 에러 수정1

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -33,8 +33,9 @@ export class ImageService {
 
     const param = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `${folder}/${Date.now().toString()}-${encodeURIComponent(
-        file.originalname,
+      Key: `${folder}/${Date.now().toString()}-${file.originalname.replace(
+        /[^a-zA-Z0-9.]/g,
+        '_',
       )}`,
       ACL: 'public-read',
       Body: file.buffer,
