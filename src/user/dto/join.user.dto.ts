3a4d890bf77987d70d65d6348a2240e3144c87aa
NHJeans <guswo1013@naver.com>
2023-10-27 16:04:59 +0900

    :sparkles: : 회원가입dto수정

--- src/user/dto/join.user.dto.ts
+++ src/user/dto/join.user.dto.ts
@@ -8,13 +8,8 @@ import {
 } from 'class-validator';
 
 export class JoinUserDto {
-  @IsEmail(
-    {},
-    {
-      message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.',
-    },
-  )
-  @IsNotEmpty({ message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
+  @IsEmail({}, { message: '이메일 형식이 아닙니다' })
+  @IsNotEmpty({ message: '이메일이 비어 있으면 안됩니다' })
   @ApiProperty({
     description: '사용자의 이메일 주소',
     example: 'user@example.com',
@@ -22,10 +17,10 @@ export class JoinUserDto {
   })
   email: string;
 
-  @IsString({ message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
-  @IsNotEmpty({ message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
+  @IsString()
+  @IsNotEmpty({ message: '닉네임이 비어 있으면 안됩니다' })
   @Matches(/^[a-zA-Z0-9\uAC00-\uD7A3]*$/, {
-    message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.',
+    message: '닉네임은 한글, 알파벳, 숫자만 포함해야 합니다',
   })
   @ApiProperty({
     description: '사용자의 닉네임',
@@ -34,13 +29,14 @@ export class JoinUserDto {
   })
   nickname: string;
 
-  @IsString({ message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
-  @IsNotEmpty({ message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
-  @MinLength(8, { message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.' })
+  @IsString()
+  @IsNotEmpty({ message: '비밀번호가 비어 있으면 안됩니다' })
+  @MinLength(8, { message: '비밀번호는 8자 이상이어야 합니다' })
   @Matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
     {
-      message: '이메일 혹은 비밀번호를 다시 한 번 확인해 주세요.',
+      message:
+        '비밀번호는 대문자, 소문자, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다',
     },
   )
   @ApiProperty({
