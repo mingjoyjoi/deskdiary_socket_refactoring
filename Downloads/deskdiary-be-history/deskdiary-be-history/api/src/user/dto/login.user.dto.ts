f3136156edc1e68e4edce8423ff9762e643573a5
mingjoyjoi <123764130+mingjoyjoi@users.noreply.github.com>
2023-11-07 20:19:12 +0900

    Delete Downloads/deskdiary-be-history/deskdiary-be-history directory

--- Downloads/deskdiary-be-history/deskdiary-be-history/api/src/user/dto/login.user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { JoinUserDto } from './join.user.dto';
-
-export class LoginUserDto extends PickType(JoinUserDto, [
-  'email',
-  'password',
-] as const) {}
