d76222b7b6c6ba84a7447585c64a1481fb2b813d
NHJeans <guswo1013@naver.com>
2023-11-09 08:47:00 +0900

    :wrench: : api main.ts파일 오류 수정

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,9 +4,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { CorsConfig, SwaggerConfig } from './config';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
-// import { RoomSeedService } from './room/room.seed.service';
+import { CorsConfig } from './config/cors.config';
+import { SwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
