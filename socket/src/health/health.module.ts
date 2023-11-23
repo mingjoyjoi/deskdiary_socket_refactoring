145a85d3bfe275f395c511de6966f6d3ae72c976
NHJeans <guswo1013@naver.com>
2023-11-24 00:18:25 +0900

    :wrench: : health폴더 삭제

--- socket/src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MongooseHealthIndicator } from './mongoose.health.indicator';
-import { HealthController } from './health.controller';
-
-@Module({
-  controllers: [HealthController],
-  providers: [MongooseHealthIndicator],
-})
-export class HealthModule {}
