7712af0f83c9d814e7a9c334110a61e66b5c32a7
mingjoy <121smmmj@gmail.com>
2023-12-16 18:38:41 +0900

    :card_file_box: : 소켓 레포지토리 예외처리

--- socket/src/gateway/room-chats.repository.ts
+++ socket/src/gateway/room-chats.repository.ts
@@ -1,22 +1,36 @@
 import { pubClient as Redis } from '../redis.adapter';
 import { Injectable, Logger } from '@nestjs/common';
 import { IRoomRequest, UserData } from './room-chats.interface';
-//:card_file_box:
+
 @Injectable()
 export class RoomchatsRepository {
   private logger: Logger = new Logger('RoomchatsRepository');
 
-  async getUserInfo(userId: number) {
-    //특정 종류의 데이터를 구분하기 위해 네임스페이스를 사용한다는데 사용법공부필요
-    return Redis.get(`user:${userId}`);
+  async getUserInfo(userId: number): Promise<string | null> {
+    try {
+      return await Redis.get(`user:${userId}`);
+    } catch (error) {
+      this.logger.error(`Failed to get user info for userId ${userId}`, error.stack);
+      throw error;
+    }
   }
 
-  async getclientInfo(clinetId: string) {
-    return Redis.get(`client:${clinetId}`);
+  async getClientInfo(clientId: string): Promise<string | null> {
+    try {
+      return await Redis.get(`client:${clientId}`);
+    } catch (error) {
+      this.logger.error(`Failed to get client info for clientId ${clientId}`, error.stack);
+      throw error;
+    }
   }
 
-  async getRoomInfo(uuid: string) {
-    return Redis.get(`room:${uuid}`);
+  async getRoomInfo(uuid: string): Promise<string | null> {
+    try {
+      return await Redis.get(`room:${uuid}`);
+    } catch (error) {
+      this.logger.error(`Failed to get room info for uuid ${uuid}`, error.stack);
+      throw error;
+    }
   }
 
   async setRoomUserAndClient(
@@ -24,54 +38,78 @@ export class RoomchatsRepository {
     clientId: string,
     roomData,
     userData: UserData,
-  ) {
-    await Redis.multi()
-      .set(`room:${uuid}`, JSON.stringify(roomData))
-      .set(`user:${userId}`, JSON.stringify(userData))
-      .set(`client:${clientId}`, uuid)
-      .exec();
-    // await Redis.set(`room:${uuid}`, JSON.stringify(roomData));
-    // await Redis.set(`user:${userId}`, JSON.stringify(userData));
-    // await Redis.set(`client:${clientId}`, uuid);
+  ): Promise<void> {
+    const multi = Redis.multi();
+
+    try {
+      multi.set(`room:${uuid}`, JSON.stringify(roomData))
+        .set(`user:${userId}`, JSON.stringify(userData))
+        .set(`client:${clientId}`, uuid);
+
+      await multi.exec();
+    } catch (error) {
+      this.logger.error(`Failed to set room, user, and client data for uuid ${uuid}`, error.stack);
+      throw error;
+    }
   }
 
-  async deleteRoom(uuid: string) {
-    await Redis.del(`room:${uuid}`);
+  async deleteRoom(uuid: string): Promise<void> {
+    try {
+      await Redis.del(`room:${uuid}`);
+    } catch (error) {
+      this.logger.error(`Failed to delete room for uuid ${uuid}`, error.stack);
+      throw error;
+    }
   }
 
-  async deleteUserAndClient(userId: number, clientId: string) {
-    await Redis.multi().del(`user:${userId}`).del(`client:${clientId}`).exec();
-    // await Redis.del(`user:${userId}`);
-    // await Redis.del(`client:${clientId}`);
+  async deleteUserAndClient(userId: number, clientId: string): Promise<void> {
+    const multi = Redis.multi();
+
+    try {
+      multi.del(`user:${userId}`).del(`client:${clientId}`);
+      await multi.exec();
+    } catch (error) {
+      this.logger.error(`Failed to delete user and client data for userId ${userId}`, error.stack);
+      throw error;
+    }
   }
+
   async setRoomDeleteUserAndClient(
-    room,
+    room, 
     uuid: string,
     userId: number,
     clientId: string,
-  ) {
-    await Redis.multi()
-      .set(`room:${uuid}`, JSON.stringify(room))
-      .del(`user:${userId}`)
-      .del(`client:${clientId}`)
-      .exec();
-    // await Redis.set(`room:${uuid}`, JSON.stringify(room));
-    // await Redis.del(`user:${userId}`);
-    // await Redis.del(`client:${clientId}`);
+  ): Promise<void> {
+    const multi = Redis.multi();
+
+    try {
+      multi.set(`room:${uuid}`, JSON.stringify(room))
+        .del(`user:${userId}`)
+        .del(`client:${clientId}`);
+
+      await multi.exec();
+    } catch (error) {
+      this.logger.error(`Failed to set room and delete user and client data for uuid ${uuid}`, error.stack);
+      throw error;
+    }
   }
 
   async deleteRoomUserAndClient(
     uuid: string,
     userId: number,
     clientId: string,
-  ) {
-    await Redis.multi()
-      .del(`room:${uuid}`)
-      .del(`user:${userId}`)
-      .del(`client:${clientId}`)
-      .exec();
-    // await Redis.del(`room:${uuid}`);
-    // await Redis.del(`user:${userId}`);
-    // await Redis.del(`client:${clientId}`);
+  ): Promise<void> {
+    const multi = Redis.multi();
+
+    try {
+      multi.del(`room:${uuid}`)
+        .del(`user:${userId}`)
+        .del(`client:${clientId}`);
+
+      await multi.exec();
+    } catch (error) {
+      this.logger.error(`Failed to delete room, user, and client data for uuid ${uuid}`, error.stack);
+      throw error;
+    }
   }
 }
