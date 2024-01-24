b1ce2823cc28910427e56661be63d75c85041807
mingjoy <121smmmj@gmail.com>
2024-01-24 21:49:37 +0900

    Room, User data interface 구체화

--- socket/src/gateway/room-chats.interface.ts
+++ socket/src/gateway/room-chats.interface.ts
@@ -4,6 +4,7 @@ export interface IRoomRequest {
   img?: string;
   userId?: number;
 }
+
 export interface IMessage {
   time?: string;
   uuid: string;
@@ -11,8 +12,30 @@ export interface IMessage {
   message: string;
   img: string;
 }
+
 export interface UserData {
-  clientId: string;
-  uuid: string;
+  clientId?: string;
+  uuid?: string;
+  nickname?: string;
+  userId?: number;
+}
+
+export interface RoomData {
+  userList: UserList;
+}
+
+export interface UserList {
+  [clientId: string]: ClientId;
+}
+
+export interface ClientId {
   nickname: string;
+  img: string;
+  userId: number;
+}
+
+export interface ForsaveRoomAndUserData {
+  iRoomRequest: IRoomRequest;
+  roomData: RoomData;
+  userData: UserData;
 }
