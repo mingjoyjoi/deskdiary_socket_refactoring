ef2ea7d0c4b376a7512e6a51e7cca9da22b04271
mingjoy <121smmmj@gmail.com>
2024-01-24 21:46:16 +0900

    소켓 필터로 axios error 처리하기

--- socket/src/filter/socket.exception.filter.ts
+++ socket/src/filter/socket.exception.filter.ts
@@ -1,10 +1,29 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Injectable,
+} from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
-import { ArgumentsHost, Catch } from '@nestjs/common';
-import { WsException } from '@nestjs/websockets';
+import { WsException, BaseWsExceptionFilter } from '@nestjs/websockets';
+import { AxiosError } from 'axios';
+import { QueryFailedError } from 'typeorm';
 
-@Catch(WsException)
-export class WsExceptionFilter extends BaseExceptionFilter {
-  catch(exception: WsException, host: ArgumentsHost) {
-    super.catch(exception, host);
+@Catch(AxiosError)
+export class WsExceptionFilter extends BaseWsExceptionFilter {
+  catch(exception: AxiosError, host: ArgumentsHost) {
+    if (exception.response) {
+      console.error('HTTP 에러 상태 코드:', exception.response.status);
+      console.error('HTTP 에러 응답 데이터:', exception.response.data);
+    } else if (exception.request) {
+      console.error('요청에 응답이 없습니다.');
+    } else {
+      console.error('요청을 보내는 중에 에러 발생:', exception.message);
+    }
+    const socket = host.switchToWs().getClient();
+    socket.emit('error-room', {
+      data: '방 나가기 요청 중 오류가 발생했습니다.',
+    });
   }
 }
